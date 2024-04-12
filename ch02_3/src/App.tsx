540dab5f2d1b4c5e5f2688c1a0fad6a3ad0385da
skcho <skcho@hist.co.kr>
2024-04-12 13:12:05 +0900

    ch02_3_1

--- ch02_3/src/App.tsx
+++ ch02_3/src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <ul>
+        <li>
+          <a href="http://www.google.com">
+            <p>go to google</p>
+          </a>
+        </li>
+      </ul>
+    )
+  }
 }
-
-export default App;
